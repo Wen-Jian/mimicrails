ee1292e6afab9329741fb64a5bd0989350f6682e
Wen-Jian <genning7@gmail.com>
2018-01-06 18:44:13 +0800

    routing中Object(大寫o）

--- lib/mimic_rails/routing.rb
+++ lib/mimic_rails/routing.rb
@@ -4,8 +4,7 @@ module MimicRails
 
 		def get_controller_and_action(env)
 
-			_, cont, action, after =
-			env["PATH_INFO"].split('/',4)
+			_, cont, action, after = env["PATH_INFO"].split('/',4)
 			cont = cont.capitalize # "People"
 			cont += "Controller" # "PeopleController"
 			[Object.const_get(cont), action]
